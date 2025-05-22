a68b3c28cd2da6a1969ff00f50446669e94596aa
Raul Silveira <raulantsilveira@gmail.com>
2025-05-22 16:50:16 -0300

    change json-server version

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({
-  static: false
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running')
-});
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`)
+})
